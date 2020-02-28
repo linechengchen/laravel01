2d8154feb81712af779616d50ecf4427e16a2b2d
CCHgit config  --global  user.name <2480402065@qq.com>
2020-02-28 16:26:37 +0800

    查看用户列表

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.default')
+@section('title', '所有用户')
+
+@section('content')
+    <div class="offset-md-2 col-md-8">
+        <h2 class="mb-4 text-center">所有用户</h2>
+        @foreach($users as $user)
+            @include('users._user')
+        @endforeach
+        <div class="mt-3">
+            {{$users->links()}}
+        </div>
+    </div>
+@stop
