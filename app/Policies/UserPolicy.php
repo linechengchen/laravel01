15c9cc35a63cbb0b77b2701cd5ed247f72539ab5
CCHgit config  --global  user.name <2480402065@qq.com>
2020-02-28 17:43:43 +0800

    管理员可删除用户

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -18,6 +18,12 @@ class UserPolicy
     {
         return $currentUser->id === $user->id;
     }
+    public  function  destroy(User $currentUser,User $user){
+        return $currentUser->is_admin && $currentUser->id !== $user->id;
+    }
+//    public  function  destroy2222(User $currentUser,User $user){
+//        return $user->name=="Summer";
+//    }
     public function __construct()
     {
         //
