f7ad6b81a7039db83cf4c920d123a39933bc9db7
CCHgit config  --global  user.name <2480402065@qq.com>
2020-03-04 17:35:25 +0800

    关注用户动态流

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Str;
 use function foo\func;
+use Auth;
 
 class User extends Authenticatable
 {
@@ -54,7 +55,11 @@ class User extends Authenticatable
     }
     //动态流数据
     public  function  feed(){
-        return $this->statuses()->orderBy('created_at','desc');
+        $user_ids = $this->followings->pluck('id')->toArray();
+        array_push($user_ids, $this->id);
+        return Status::whereIn('user_id', $user_ids)
+            ->with('user')
+            ->orderBy('created_at', 'desc');
     }
     public  function followers(){
         return $this->belongsToMany(User::Class, 'followers', 'user_id', 'follower_id');
